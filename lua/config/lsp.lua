0547db0ca279078969447124175d01d3ae960c3f
Rob Carruthers <12486527+rob-carruthers@users.noreply.github.com>
2026-03-14 13:42:51 +0000

    Make diagnostics in gutter more like helix

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,2 +1,19 @@
+vim.diagnostic.config({
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = '●',
+      [vim.diagnostic.severity.WARN] = '●',
+      [vim.diagnostic.severity.HINT] = '●',
+      [vim.diagnostic.severity.INFO] = '●',
+    },
+    linehl = {},
+    numhl = {
+      [vim.diagnostic.severity.ERROR] = 'ErrorMsg',
+      [vim.diagnostic.severity.WARN] = 'WarningMsg',
+    },
+  },
+  virtual_lines = true,
+  virtual_text = false
+})
 vim.lsp.enable('ruff')
 vim.lsp.enable('ty')
