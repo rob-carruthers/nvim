aea8ce1f2054c7d0c310c9e141f148e329ca74fd
Rob Carruthers <12486527+rob-carruthers@users.noreply.github.com>
2026-03-14 20:09:17 +0000

    Update setup call for nvim-treesitter

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -2,10 +2,12 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
-    require("nvim-treesitter.config").setup({
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
     require("nvim-treesitter").install({ "bash", "lua", "python" })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function()
+        pcall(vim.treesitter.start)
+      end,
+    })
   end,
 }
