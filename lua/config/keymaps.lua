c6b2a7fbe966614305596f38b935a8ad54e47383
Rob Carruthers <12486527+rob-carruthers@users.noreply.github.com>
2026-03-14 20:25:53 +0000

    Update keymaps with descriptions

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,14 +2,14 @@
 vim.keymap.set("n", "U", "<C-r>")
 
 -- Line navigation
-vim.keymap.set({ "n", "v" }, "gh", "0")
-vim.keymap.set({ "n", "v" }, "gs", "^")
-vim.keymap.set({ "n", "v" }, "gl", "$")
-vim.keymap.set({ "n", "v" }, "ge", "G")
+vim.keymap.set({ "n", "v" }, "gh", "0", { desc = "Go to beginning of line" })
+vim.keymap.set({ "n", "v" }, "gs", "^", { desc = "Go to beginning of content of line" })
+vim.keymap.set({ "n", "v" }, "gl", "$", { desc = "Go to end of line" })
+vim.keymap.set({ "n", "v" }, "ge", "G", { desc = "Go to end of file" })
 
 -- LSP actions, ported from helix
-vim.keymap.set("n", "gd", vim.lsp.buf.definition)
-vim.keymap.set("n", "gr", vim.lsp.buf.references)
-vim.keymap.set("n", "<leader>k", vim.lsp.buf.hover)
-vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename)
-vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action)
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Go to references" })
+vim.keymap.set("n", "<leader>k", vim.lsp.buf.hover, { desc = "Hover" })
+vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { desc = "Rename symbol" })
+vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action, { desc = "Code actions" })
