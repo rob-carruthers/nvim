c1dfe9a8297e40683f0634132be4d225a511271d
Rob Carruthers <12486527+rob-carruthers@users.noreply.github.com>
2026-03-14 13:29:09 +0000

    Add some initial keymaps

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,15 @@
+-- Redo
+vim.keymap.set("n", "U", "<C-r>")
+
+-- Line navigation
+vim.keymap.set("n", "gh", "0")
+vim.keymap.set("n", "gs", "^")
+vim.keymap.set("n", "gl", "$")
+vim.keymap.set("n", "ge", "G")
+
+-- LSP actions, ported from helix
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "gr", vim.lsp.buf.references)
+vim.keymap.set("n", "<leader>k", vim.lsp.buf.hover)
+vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename)
+vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action)
