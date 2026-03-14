d1c9c03f15a147b8078a22bcaffde4f890114ddf
Rob Carruthers <12486527+rob-carruthers@users.noreply.github.com>
2026-03-14 20:09:29 +0000

    Add indent-blankline + rainbow-delimiters

---
+++ lua/plugins/indent-blankline.lua
@@ -0,0 +1,28 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  dependencies = { "HiPhish/rainbow-delimiters.nvim" },
+  config = function()
+    local highlight = {
+      "RainbowDelimiterRed",
+      "RainbowDelimiterYellow",
+      "RainbowDelimiterBlue",
+      "RainbowDelimiterOrange",
+      "RainbowDelimiterGreen",
+      "RainbowDelimiterViolet",
+      "RainbowDelimiterCyan",
+    }
+    local hooks = require "ibl.hooks"
+    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+      vim.api.nvim_set_hl(0, "RainbowDelimiterRed", { fg = "#E06C75" })
+      vim.api.nvim_set_hl(0, "RainbowDelimiterYellow", { fg = "#E5C07B" })
+      vim.api.nvim_set_hl(0, "RainbowDelimiterBlue", { fg = "#61AFEF" })
+      vim.api.nvim_set_hl(0, "RainbowDelimiterOrange", { fg = "#D19A66" })
+      vim.api.nvim_set_hl(0, "RainbowDelimiterGreen", { fg = "#98C379" })
+      vim.api.nvim_set_hl(0, "RainbowDelimiterViolet", { fg = "#C678DD" })
+      vim.api.nvim_set_hl(0, "RainbowDelimiterCyan", { fg = "#56B6C2" })
+    end)
+    hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+    require("ibl").setup { scope = { highlight = highlight }, indent = { highlight = highlight } }
+  end
+}
