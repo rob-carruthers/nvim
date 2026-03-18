f75bdf35ea906dc6264b6624ee3c603151a73b18
Rob Carruthers <12486527+rob-carruthers@users.noreply.github.com>
2026-03-18 21:41:17 +0000

    Customize lualine appearance

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,7 +3,38 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     require("lualine").setup({
-      options = { theme = "nord" },
+      options = {
+        theme = "nord",
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = {
+          "branch",
+        },
+        lualine_c = {
+          {
+            "diagnostics",
+            symbols = { error = "● ", warn = "● ", info = "● ", hint = "● " },
+          },
+        },
+        lualine_x = { "encoding", "filetype" },
+        lualine_y = {},
+        lualine_z = { "location" },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {},
+      },
+      tabline = {},
+      winbar = {},
+      inactive_winbar = {},
+      extensions = {},
     })
   end,
 }
